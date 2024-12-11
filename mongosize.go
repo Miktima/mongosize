090a3626770db5ee1beb94c1ec3dde1f39f3f9db
Тимофеев Михаил Константинович <m.timofeev@ria.ru>
2024-12-11 16:43:18 +0300

    dongrade mongodb driver version

--- mongosize.go
+++ mongosize.go
@@ -1,32 +1,46 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
-	"net/url"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	var mode string
-	var parameter string
+	var connection string
 
 	// Ключи для командной строки
-	flag.StringVar(&mode, "mode", "", "mode of the check [urldecode, urlencode]")
-	flag.StringVar(&parameter, "p", "", "incoming value (parameter)")
+	flag.StringVar(&connection, "connection", "", "URI of Mongodb server")
 
 	flag.Parse()
 
-	if mode == "urldecode" {
-		query, err := url.QueryUnescape(parameter)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("Result: ", query)
-	} else if mode == "urlencode" {
-		query := url.QueryEscape(parameter)
-		fmt.Println("Result: ", query)
-	} else {
-		fmt.Println("ERROR: unrecognized mode, valid = [urldecode, urlencode]")
+	//var collection *mongo.Collection
+	var dbs []string
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI(connection)
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
 	}
+	dbs, err = client.ListDatabaseNames(
+		ctx,
+		bson.D{primitive.E{Key: "empty", Value: false}})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for _, db := range dbs {
+		fmt.Println(db)
+	}
+
 }
